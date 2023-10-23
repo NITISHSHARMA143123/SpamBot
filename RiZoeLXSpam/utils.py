0fb7e4d9634d4154518db2e1b13007b6a7912278
Venom <jasicaxaayu@gmail.com>
2023-10-24 03:02:37 +0530

    Update utils.py

--- RiZoeLXSpam/utils.py
+++ RiZoeLXSpam/utils.py
@@ -8,14 +8,14 @@ import inspect
 import re
 
 def load_plugins(plugin_name):
-    path = Path(f"VenomXSpam/plugins/{plugin_name}.py")
-    name = "VenomXSpam.plugins.{}".format(plugin_name)
+    path = Path(f"RiZoeLXSpam/plugins/{plugin_name}.py")
+    name = "RiZoeLXSpam.plugins.{}".format(plugin_name)
     spec = importlib.util.spec_from_file_location(name, path)
     load = importlib.util.module_from_spec(spec)
     load.logger = logging.getLogger(plugin_name)
     spec.loader.exec_module(load)
-    sys.modules["VenomXSpam.plugins." + plugin_name] = load
-    print("Venom X Spam has Imported Module: " + plugin_name)
+    sys.modules["RiZoeLXSpam.plugins." + plugin_name] = load
+    print("RiZoeL X Spam has Imported Module: " + plugin_name)
 
 async def edit_or_reply(event, text):
     if event.sender_id in SUDO_USERS:
@@ -24,4 +24,3 @@ async def edit_or_reply(event, text):
             return await reply_to.reply(text)
         return await event.reply(text)
     return await event.edit(text)
-
